f222bd6fb9bf9c89aa2fc8f8621df1d5fa48cccc
Cesar <rogercesar778876@gmail.com>
2017-10-13 11:47:03 -0400

    add list.c

---
+++ list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {char *s, struct node *next};
+
+void print_list(struct node *){
+  printf("[");
+  while (node.next != 0){
+    printf("%s, ", node.s);
+    node++;
+    
+  }
+  printf("]\n");
+}
+
+int main(){
+    struct node n1 = 
+}
